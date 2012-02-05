28f07334a841f0296daa3eb4d8a3305948d3e41c
Gregor Aisch <contact@vis4.net>
2012-02-05 21:30:33 +0100

    initial commit

---
+++ README.rst
@@ -0,0 +1,8 @@
+# fuzzymatch
+
+Interactive command line utility to merge two tables based on text similarity of two columns.
+
+```
+fuzzymatch from-table.csv to-table.csv 
+```
+
---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+long_desc = """
+
+"""
+
+
+setup(
+    name='fuzzymatch',
+    version='0.1',
+    description="Command line util for merging two tables based on text similarity measurements.",
+    long_description=long_desc,
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        ],
+    keywords='',
+    author='Gregor Aisch',
+    author_email='gregor.aisch@okfn.org',
+    url='http://labs.okfn.org',
+    license='MIT',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    namespace_packages=[],
+    include_package_data=False,
+    zip_safe=False,
+    install_requires=[
+    ],
+    tests_require=[],
+    entry_points=\
+    """
+""",
+)
