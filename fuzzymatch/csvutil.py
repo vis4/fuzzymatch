9ad31b350c2f15d1fc6bfbecc3083b0e0d9f4ef5
Gregor Aisch <contact@vis4.net>
2012-02-27 11:53:53 +0100

    fixed bug in automated csv dialect recognition

--- fuzzymatch/csvutil.py
+++ fuzzymatch/csvutil.py
@@ -1,20 +1,20 @@
 """
 
 """
-import unicodecsv as ucsv
+import ucsv
 import csv
 
-def guess_dialect(file):
+def guess_dialect(filename):
 	"""tries to guess the dialect of csv files"""
 	best = ''
 	max_columns = 0
-	file = open(file,'r')
 	for dialect in csv.list_dialects():
-		reader = ucsv.reader(file, dialect=dialect)
-		header = reader.next()
+		file = open(filename, 'r')
+		rd = ucsv.reader(file, dialect=dialect)
+		header = rd.next()
 		if len(header) > max_columns:
 			max_columns = len(header)
 			best = dialect
-	file.close()
+		file.close()
 	return best
 
